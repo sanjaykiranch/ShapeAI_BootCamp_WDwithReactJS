30c2c4185965157027fece9fac441c7bee3d5627
sanjaykiranch <sanjusanjaykiran890@gmail.com>
2021-06-27 18:28:59 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div>
+      <div className="note">
+        <h1>Java Script and React.js</h1>
+        <p>
+          this is a 7 days long Free Bootcamp on basics of React.js and
+          JavaScript
+        </p>
+      </div>
+      <div className="note">
+        <h1>Java Script and Node.js</h1>
+        <p>
+          this is a 7 days long Free Bootcamp on basics of Node.js and
+          JavaScript
+        </p>
+      </div>
+      <div className="note">
+        <h1>Python and Data Analyst</h1>
+        <p>
+          this is a 7 days long Free Bootcamp on basics of Data Analysis using
+          python
+        </p>
+      </div>
+      <div className="note">
+        <h1>Python</h1>
+        <p>This is a 7 days long Free Bootcamp on basics of Python</p>
+      </div>
+    </div>
+  );
+}
+
+export default Info;
